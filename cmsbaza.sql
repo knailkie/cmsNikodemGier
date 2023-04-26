4cb968b9fd4390907234bc391aa3f172dbcd30f2
knailkie <100939071+knailkie@users.noreply.github.com>
2023-04-26 15:10:09 +0200

    Add files via upload

---
+++ cmsbaza.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 26 Kwi 2023, 15:06
+-- Wersja serwera: 10.4.24-MariaDB
+-- Wersja PHP: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `likes`
+--
+
+CREATE TABLE `likes` (
+  `post_id` int(96) NOT NULL,
+  `user_id` int(96) NOT NULL,
+  `value` int(96) NOT NULL,
+  `id` int(96) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `likes`
+--
+
+INSERT INTO `likes` (`post_id`, `user_id`, `value`, `id`) VALUES
+(2, 1, -1, 49),
+(3, 1, -1, 51),
+(1, 2, 1, 71),
+(2, 2, 1, 74),
+(3, 2, 1, 78),
+(1, 4, 1, 83),
+(1, 3, 1, 84),
+(4, 3, 1, 86),
+(1, 1, -1, 91);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(96) NOT NULL,
+  `timestamp` date NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `authorName` varchar(200) NOT NULL,
+  `removed` varchar(255) NOT NULL,
+  `authorId` int(96) NOT NULL,
+  `imageUrl` varchar(255) NOT NULL,
+  `userId` int(96) NOT NULL,
+  `ip` varchar(255) NOT NULL
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(96) NOT NULL,
+  `email` varchar(96) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(96) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+
+--
+-- AUTO_INCREMENT dla tabeli `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(96) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(96) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
